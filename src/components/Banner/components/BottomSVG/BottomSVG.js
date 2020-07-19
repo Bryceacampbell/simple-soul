0510fbc058bbcbe0288568162a5825602b3f4f2c
Jonathan Hansen <kriah0872@gmail.com>
2020-07-18 23:16:20 -0700

    banner. not rendering children yet

---
+++ src/components/Banner/components/BottomSVG/BottomSVG.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BottomSVG = () => {
+  return (
+    <svg viewBox="0 0 1364 65.27">
+      <path
+        d="M1364,0H0V41c61.55-5.69,125.47-10,191.64-12.5A3569.53,3569.53,0,0,1,681,43.5a2551.67,2551.67,0,0,0,427.31,20A2546,2546,0,0,0,1364,41Z"
+        style={{ fill: "var(--simple-blue)" }}
+      />
+    </svg>
+  );
+};
+
+export default BottomSVG;
---
+++ src/components/Banner/components/TopSVG/TopSVG.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TopSVG = () => {
+  return (
+      <svg viewBox="0 0 1364 65.27">
+            <path
+              d="M0,65.27H1364v-41c-61.55,5.68-125.47,10-191.64,12.5A3569.53,3569.53,0,0,1,683,21.77,2545.94,2545.94,0,0,0,0,24.27Z"
+              style={{fill: 'var(--simple-blue)'}}
+            />
+      </svg>
+  );
+};
+
+export default TopSVG;
