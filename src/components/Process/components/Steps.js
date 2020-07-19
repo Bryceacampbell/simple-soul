5ec20bbf23d0fb407047b72f5ac5e7f747ee0bfd
Jonathan Hansen <kriah0872@gmail.com>
2020-07-19 13:37:54 -0700

    custom scan icon and centered other icons

--- src/components/Process/components/Steps.js
+++ src/components/Process/components/Steps.js
@@ -1,46 +1,57 @@
-import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Hair from '../../SvgIcons/Hair/Hair';
-import NinetyDay from '../../SvgIcons/NinetyDay/NinetyDay';
-import PieChartIcon from '@material-ui/icons/PieChart';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Hair from "../../SvgIcons/Hair/Hair";
+import NinetyDay from "../../SvgIcons/NinetyDay/NinetyDay";
+import PieChartIcon from "@material-ui/icons/PieChart";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import ScanIcon from '../../SvgIcons/Scan/Scan'
 const useStyles = makeStyles((theme) => ({
-    roundedDiv: {
-        borderRadius: '50%',
-        background: '#00707C',
-        width: 125,
-        height: 125,
-        marginRight: 'auto',
-        marginLeft: 'auto',
-        marginBottom: 10,
-        alignItems: 'center'
-    },
+  roundedDiv: {
+    borderRadius: "50%",
+    background: "#00707C",
+    width: 125,
+    height: 125,
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginBottom: 10,
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  iconFont: {
+    fontSize: "3.5em",
+    color: 'white'
+  },
 }));
 
-const steps = [
-    { id: 1, name: 'Collect' , icon: <Hair color='#fff' width='50%'/>},
-    { id: 2, name: 'Analyze' },
-    { id: 3, name: 'Results', icon: <PieChartIcon /> },
-    { id: 4, name: 'Optimize Wellness', icon: <AssignmentIcon /> },
-    { id: 5, name: '90 Day Review', icon: <NinetyDay color='#fff' width='50%'/> },
-];
-
 const Steps = () => {
-    const classes = useStyles();
-    return (
-        <Grid container spacing={6} direction='row' style={{ textAlign: 'center' }}>
-            {steps.map((step => (
-                <Grid item xs={4} md key={step.id}>
-                    <div className={classes.roundedDiv} >
-                        {step.icon}
-                    </div>
-                    <Typography variant='h5'>{step.name}</Typography>
-                </Grid>
-            )))}
+  const classes = useStyles();
+  const steps = [
+    { id: 1, name: "Collect", icon: <Hair color="#fff" width="3em" /> },
+    { id: 2, name: "Analyze", icon: <ScanIcon width='3rem' color='#fff' /> },
+    { id: 3, name: "Results", icon: <PieChartIcon className={classes.iconFont} /> },
+    {
+      id: 4,
+      name: "Optimize Wellness",
+      icon: <AssignmentIcon className={classes.iconFont} />,
+    },
+    {
+      id: 5,
+      name: "90 Day Review",
+      icon: <NinetyDay color="#fff" width="3em" />,
+    },
+  ];
+  return (
+    <Grid container spacing={6} direction="row" style={{ textAlign: "center" }}>
+      {steps.map((step) => (
+        <Grid item xs={4} md key={step.id}>
+          <div className={classes.roundedDiv}>{step.icon}</div>
+          <Typography variant="h5">{step.name}</Typography>
         </Grid>
-    )
+      ))}
+    </Grid>
+  );
 };
 
 export default Steps;
