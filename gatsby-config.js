40a3fbcc7a5f533769aad3086534463f753b6067
bryceacampbell <bryceacampbell7@gmail.com>
2020-07-20 20:44:59 -0700

    install gatsby material ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: 'gatsby-plugin-material-ui',
+      // If you want to use styled components you should change the injection order.
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
