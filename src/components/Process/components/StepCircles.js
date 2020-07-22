39eb2d2e53612e6550c3ad80dc55ebd3d5f1f31d
Jonathan Hansen <kriah0872@gmail.com>
2020-07-21 22:00:13 -0700

    added hidden step circles for mobile/desktop

---
+++ src/components/Process/components/StepCircles.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const StepCircles = ({ icon, name }) => {
+  const useStyles = makeStyles(() => ({
+    roundedDiv: {
+      borderRadius: "50%",
+      background: "#00707C",
+      width: "6rem",
+      height: "6rem",
+      marginRight: "auto",
+      marginLeft: "auto",
+      marginBottom: 10,
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.roundedDiv}>{icon}</div>
+      <h5
+        style={{
+          margin: "0",
+          fontFamily: "Montserrat",
+          fontSize: "1.5em",
+          textAlign: 'center',
+          fontWeight: '200'
+        }}
+      >
+        {name}
+      </h5>
+    </>
+  );
+};
+
+export default StepCircles;
