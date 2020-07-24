8db39549c5ae00c990a0e9c22646ddcd01e50805
Jonathan Hansen <kriah0872@gmail.com>
2020-07-23 21:58:17 -0700

    contact cards, colors, etc...

--- src/components/Contact/components/InfoSection/InfoSection.js
+++ src/components/Contact/components/InfoSection/InfoSection.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import InfoCard from './InfoCard'
-import {Grid} from '@material-ui/core'
+import React from "react";
+import InfoCard from "./InfoCard";
+import { Grid } from "@material-ui/core";
+import FamilyIcon from "../../../SvgIcons/Family/FamilyIcon";
+import HusbandWifeIcon from "../../../SvgIcons/HusbandWife/HusbandWifeIcon";
 
 const InfoSection = () => {
+  const infoCards = [
+    { icon: <FamilyIcon width='4rem' color='var(--simple-blue' />, header: "Family Discounts", text: "lorem" },
+    { icon: <HusbandWifeIcon width='4rem' color='var(--simple-blue' />, header: "Couples Discounts", text: "lorem" },
+  ];
   return (
-    <Grid spacing={5} style={{marginTop: '.5em'}} container justify='center'>
-      <Grid item><InfoCard /></Grid>
-      <Grid item><InfoCard /></Grid>
+    <Grid spacing={5} style={{ marginTop: ".5em" }} container justify="center">
+      {infoCards.map(card => (
+        <Grid item xs={12}>
+          <InfoCard icon={card.icon} header={card.header} text={card.text} />
+          </Grid>
+      ))}
     </Grid>
-  )
-}
+  );
+};
 
-export default InfoSection
+export default InfoSection;
