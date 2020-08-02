c151ba9b61bf5d06991a9f5e6a45179ccd655798
Jonathan Hansen <kriah0872@gmail.com>
2020-08-02 11:48:37 -0700

    form component all one component

--- src/components/Contact/components/ContactForm/ContactForm.js
+++ src/components/Contact/components/ContactForm/ContactForm.js
@@ -1,18 +1,69 @@
 import React from "react";
-import { Grid, Hidden } from "@material-ui/core";
-import FormInputs from '../FormInputs'
+import { Grid, Hidden, TextField, Button } from "@material-ui/core";
 const ContactForm = () => {
-
+  const inputs = [
+    'Name',
+    'Phone',
+    'Message'
+  ]
   return (
     <Grid item>
       <form method="post" data-netlify="true" data-netlify-honeypot="bot-field" name="contact">
         <Hidden smUp>
-        <Grid justify='center' container>
-          <FormInputs />
-        </Grid>
+          <Grid justify='center' container>
+            <div style={{ display: 'contents' }}>
+              <input type='hidden' name='form-name' value='contact' />
+              {inputs.map(li => (
+                <TextField
+
+                  style={{ width: "80%", marginBottom: "1.5em" }}
+                  size="medium"
+                  name={li}
+                  label={li}
+                  key={li}
+                  mutliline={li === 'Message'}
+                ></TextField>
+              ))}
+              <div style={{ width: "80%" }}>
+                <Button type='submit'
+                  style={{
+                    float: "right",
+                    backgroundColor: "var(--simple-blue)",
+                    color: "#fff",
+                  }}
+                >
+                  Send
+            </Button>
+              </div>
+            </div>
+          </Grid>
         </Hidden>
         <Hidden only='xs'>
-          <FormInputs />
+          <div style={{ display: 'contents' }}>
+            <input type='hidden' name='form-name' value='contact' />
+            {inputs.map(li => (
+              <TextField
+
+                style={{ width: "80%", marginBottom: "1.5em" }}
+                size="medium"
+                name={li}
+                label={li}
+                key={li}
+                mutliline={li === 'Message'}
+              ></TextField>
+            ))}
+            <div style={{ width: "80%" }}>
+              <Button type='submit'
+                style={{
+                  float: "right",
+                  backgroundColor: "var(--simple-blue)",
+                  color: "#fff",
+                }}
+              >
+                Send
+            </Button>
+            </div>
+          </div>
         </Hidden>
       </form>
     </Grid>
--- src/components/Contact/components/FormInputs.js
+++ src/components/Contact/components/FormInputs.js
@@ -1,36 +1,9 @@
 import React from 'react'
 import { TextField, Button } from "@material-ui/core";
 const FormInputs = () => {
-  const inputs = [
-    'Name',
-    'Phone',
-    'Message'
-  ]
+  
   return (
-    <div style={{ display: 'contents' }}>
-      <input type='hidden' name='form-name' value='contact' />
-      {inputs.map(li => (
-        <TextField
-
-          style={{ width: "80%", marginBottom: "1.5em" }}
-          size="medium"
-          name={li}
-          label={li}
-          key={li}
-        ></TextField>
-      ))}
-      <div style={{ width: "80%" }}>
-        <Button type='submit'
-          style={{
-            float: "right",
-            backgroundColor: "var(--simple-blue)",
-            color: "#fff",
-          }}
-        >
-          Send
-            </Button>
-      </div>
-    </div>
+    
   )
 }
 
