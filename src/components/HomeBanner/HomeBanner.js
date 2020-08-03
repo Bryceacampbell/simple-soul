be5e719b9724089af60df971c1f21fc5b2db233a
bryceacampbell <bryceacampbell7@gmail.com>
2020-08-03 09:57:21 -0700

    adding link to schedule btn

--- src/components/HomeBanner/HomeBanner.js
+++ src/components/HomeBanner/HomeBanner.js
@@ -1,34 +1,37 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import { Grid, Container, Typography, Button, Box } from "@material-ui/core";
+
 const HomeBanner = () => {
   return (
     <Banner id="process">
       <Container>
-        <Grid display='flex' direction='column' alignItems='center' justify='center' style={{height: '100%'}} container>
-              <Typography
-                style={{ color: "white" }}
-                align="center"
-                variant="h6"
-              >
-                Want to learn more?
+        <Grid display='flex' direction='column' alignItems='center' justify='center' style={{ height: '100%' }} container>
+          <Typography
+            style={{ color: "white" }}
+            align="center"
+            variant="h6"
+          >
+            Want to learn more?
               </Typography>
-              <Typography
-                style={{ color: "white" }}
-                align="center"
-                variant="h6"
-                gutterBottom
-              >
-                Schedule an appointment today!
+          <Typography
+            style={{ color: "white" }}
+            align="center"
+            variant="h6"
+            gutterBottom
+          >
+            Schedule an appointment today!
               </Typography>
-              <Box display="flex" justifyContent="center">
-                <Button
-                  style={{ color: "white", borderColor: "#ccc" }}
-                  variant="outlined"
-                >
-                  Schedule Appointment
+          <Box display="flex" justifyContent="center">
+            <a target='_blank' href='https://square.site/book/2TM3B1F3R9Z9C/simple-soul-gilbert-az' style={{textDecoration: 'none'}}>
+              <Button
+                style={{ color: "white", borderColor: "#ccc" }}
+                variant="outlined"
+              >
+                Schedule Appointment
                 </Button>
-            </Box>
+            </a>
+          </Box>
         </Grid>
       </Container>
     </Banner>
