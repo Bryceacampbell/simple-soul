a3fcb77def0e9147e863cb083307cf3c0a234cbf
bryceacampbell <bryceacampbell7@gmail.com>
2021-03-24 15:01:49 -0700

    fixing merge conflict

--- src/components/HomeBanner/HomeBanner.js
+++ src/components/HomeBanner/HomeBanner.js
@@ -4,22 +4,15 @@ import { Grid, Container, Typography } from "@material-ui/core";
 import ScheduleModal from '../ScheduleModal/ScheduleModal';
 
 const HomeBanner = () => {
-  const classes = useStyles();
   return (
     <Banner>
       <Container>
-        <Grid
-          style={{
-            height: "100%",
-            width: "80%",
-            margin: "0 auto",
-            display: "flex",
-            flexDirection: "column",
-          }}
-          item
-          xs={12}
-        >
-          <Typography style={{ color: "white" }} align="center" variant="h6">
+        <Grid display='flex' direction='column' alignItems='center' justify='center' style={{ height: '100%' }} container>
+          <Typography
+            style={{ color: "white" }}
+            align="center"
+            variant="h6"
+          >
             Want to learn more?
           </Typography>
           <Typography
